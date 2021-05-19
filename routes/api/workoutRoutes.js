b391177aa80200689f61cc2314eed290b2ce1fcf
inknsharps <nchen25@gmail.com>
2021-05-18 23:48:52 -0400

    add placeholders for api endpoints

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,3 +1,24 @@
 const router = require("express").Router();
 
+// Get the last workout here
+router.get("/", (req, res) => {
+
+});
+
+// Get workouts by range 
+// Double check if this breaks with the exercise put route...
+router.get("/range", (req, res) => {
+
+});
+
+// Add a new workout here
+router.post("/", (req, res) => {
+
+});
+
+// Add an exercise to a workout (by ID) here???
+router.put("/:id", (req, res) => {
+
+});
+
 module.exports = router;

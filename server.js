7695b03c09b4668cb6f05b589c71b41907dddc6c
inknsharps <nchen25@gmail.com>
2021-05-18 23:24:52 -0400

    update routing to work locally

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
     console.log(`Now listening on PORT ${PORT}!`)

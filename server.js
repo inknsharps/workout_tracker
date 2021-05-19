31ef136a7ef98fe299263c40b0dbdbc0e6156f46
inknsharps <nchen25@gmail.com>
2021-05-18 22:45:42 -0400

    add base server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+    console.log(`Now listening on PORT ${PORT}!`)
+});

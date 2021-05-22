de97addb2e95897d962262b2568fc02d28a9ce42
inknsharps <nchen25@gmail.com>
2021-05-22 00:17:35 -0400

    refactor range and homepage routes to make use of Model.aggregate

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -2,31 +2,59 @@ const router = require("express").Router();
 const Workout = require("../../models/Workout");
 
 // Get the last workout here
-// We first sort all the data so that the latest ID is placed up the top
-// Then use .limit() to only return one result
+// This makes use of the Model.aggregate() function because we have to add properties to the document
+// We first use $match with an empty object to just grab all the documents
+// Then we sort all the data so that the latest ID is placed up the top
+// Then we use $limit to only return one result
+// Finally, we use $addFields to add a new property to the document, and use the $sum to get back the total duration (remember, exercises is an array of objects, so we have to access duration by using dot notation)
 router.get("/", async (req, res) => {
-    Workout.find({})
-        .sort({ _id: 1})
-        .then(lastWorkout => {
-            res.json(lastWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+    try {
+        const lastWorkout = await Workout.aggregate([
+            {
+                $match: {}
+            },
+            {
+                $sort: { _id: -1 }
+            },
+            {
+                $limit: 1
+            },
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration"}
+                }
+            }
+        ])
+        res.json(lastWorkout);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 // Get workouts by range 
 // Double check if this breaks with the exercise put route...
-router.get("/range", (req, res) => {
-    Workout.find({})
-        .sort({ _id: -1})
-        .limit(7)
-        .then(lastSevenWorkouts => {
-            res.json(lastSevenWorkouts);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+router.get("/range", async (req, res) => {
+    try {
+        const lastWorkout = await Workout.aggregate([
+            {
+                $match: {}
+            },
+            {
+                $sort: { _id: -1 }
+            },
+            {
+                $limit: 7
+            },
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration"}
+                }
+            }
+        ])
+        res.json(lastWorkout);
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 // Add a new workout here

d05e841c3ca39f77c113259b5e1d93c418065675
inknsharps <nchen25@gmail.com>
2021-05-30 09:49:06 -0400

    fix routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const workoutRoutes = require("./workoutroutes.js");
+const workoutRoutes = require("./workoutRoutes");
 
 router.use("/workouts", workoutRoutes);
 
